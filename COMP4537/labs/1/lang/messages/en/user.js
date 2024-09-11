dbeab50ef87b95b31e3b4d6978e71df330eb5856
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2024-09-10 21:26:08 -0700

    Finish landing page

--- COMP4537/labs/1/lang/messages/en/user.js
+++ COMP4537/labs/1/lang/messages/en/user.js
@@ -2,7 +2,9 @@ const Dictionary = {
     Labels: {
         IndexTitle: 'Lab 1: JSON, Object Constructor, localStorage',
         CourseNum: 'COMP 4537',
-        IndexSignature: 'by: Lucas Angelozzi (A01270381)'
+        IndexSignature: 'by: Lucas Angelozzi (A01270381)',
+        Reader: 'Reader',
+        Writer: 'Writer',
     },
     ErrorMessages: {
     },
